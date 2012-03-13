68b7f372da45ac2215885126210e99d928da44bc
Edwin Amsler <EdwinGuy@GMail.com>
2012-03-13 00:14:17 -0500

    Add a couple more examples

--- shell/bases.sh
+++ shell/bases.sh
@@ -1,6 +1,22 @@
 # Since I never remember the stupid syntax, here it be
 
+# For loops
+echo "For loopage"
 for a in 1 2 3 4 5 6 7 8 9 0
 do
 	echo $a
+done
+
+echo
+echo "Single Line"
+# Single line goodness
+for a in `ls`; do echo "File: $a"; done
+
+echo
+echo "Do the math while enjoying life"
+a=5
+while [[ a -gt 2 ]]
+do
+	echo $a
+	a=$(($a - 1))
 done
