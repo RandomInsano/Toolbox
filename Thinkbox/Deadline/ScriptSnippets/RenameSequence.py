1e39822e862271eea2a678daf3d1c0d47e13f961
Edwin Amsler <EdwinGuy@GMail.com>
2012-03-20 12:02:15 -0500

    Quick theory example script for a client.
    I don't really have time to flesh this out for them, so hopefully that's enough to get them going

---
+++ Thinkbox/Deadline/ScriptSnippets/RenameSequence.py
@@ -0,0 +1,40 @@
+# This is a starting point for a script to shift frame range numbering
+# Ex: My output files start at frame 50 when they should be named to start at from 1
+
+# Note: This hasn't been debugged. The Regex will work though.
+#    THERE'S NO GUARANTEE THIS WILL WORK! TEST AND DEBUG!
+#    This algorithm assumes no item in the range is missing
+#      Ex: In a range from frame 50 to 100, frame 75 can't be missing
+
+import re
+
+# What's missing:
+# - Find all files ending in four or more numbers
+# - Sort them (assuming all are named the same save those numbers)
+
+# This is where the file search should go
+
+# This is where the sort should go
+
+# This should be the first file in the list instead of a constant
+filename = "sample file of goodness 0009.tga"
+
+# Grab three groups:
+# First group:  everything up to the second group
+# Second group: everything that is a number (0-9 contiguous)
+# Last group:   everything that isn't a number, to the end
+# Note: 
+#   The first capture group only works because of the '$'
+#   Without it, it would faile.
+m = re.search("(.*?)([0-9]*)([^0-9]*)$", filename)
+filename = m.group(0)
+startnum = m.group(1)
+fileext  = m.group(3)
+
+filecount = 0 # The number of files we'll be renaming
+
+for newnum in range(1 to filecount):
+	oldnum = newnum + startnum
+	old = filename + oldnum + fileext
+	new = filename + newnum + fileext 
+	os.rename(old, new)
