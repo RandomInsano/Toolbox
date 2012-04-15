552a4b72b477204ffbe2fbc5a630e8ee7ede8ccb
Edwin Amsler <EdwinGuy@GMail.com>
2012-04-15 12:51:33 -0500

    Derp! Forgot leading colons.

--- shell/getIPv4in6.sh
+++ shell/getIPv4in6.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Mostly just a sed example for grabbing IP addresses. This'll only work on "ffff:10.0.0.50" style addresses
-cat $1 | sed -ne "s/.*\(ffff:[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/(\1)/p" | sort | uniq
+# Mostly just a sed example for grabbing IP addresses. This'll only work on "::ffff:10.0.0.50" style addresses
+cat $1 | sed -ne "s/.*\(::ffff:[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/(\1)/p" | sort | uniq
