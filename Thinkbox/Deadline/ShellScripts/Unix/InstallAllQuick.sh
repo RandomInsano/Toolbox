abe0f0b649a8be1dfa2f5ed8261180a6b915162a
Edwin Amsler <EdwinGuy@GMail.com>
2012-04-26 15:30:23 -0500

    Start the auto-installer script

---
+++ Thinkbox/Deadline/ShellScripts/Unix/InstallAllQuick.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /tmp
+
+wget http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5147014-april-17-2012/Deadline_Linux_Installers_x86_64_5_1_47014.tar
+
+tar xf Deadline_Linux_Installers 
