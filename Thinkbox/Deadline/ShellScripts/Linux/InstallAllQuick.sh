765398d3548ed2a6f8febfd08db7430e43e01c4b
Edwin Amsler <EdwinGuy@GMail.com>
2012-05-01 11:40:50 -0500

    Only delete if it went well

--- Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
+++ Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
@@ -4,10 +4,7 @@ pushd /tmp
 
 curl -L http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5147014-april-17-2012/Deadline_Linux_Installers_x86_64_5_1_47014.tar | tar x
 
-DeadlineRepos*
-rm DeadlineRepos*
-
-DeadlineClient*
-rm DeadlineClient*
+DeadlineRepos*	&& rm DeadlineRepos*
+DeadlineClient*	&& rm DeadlineClient*	
 
 popd
