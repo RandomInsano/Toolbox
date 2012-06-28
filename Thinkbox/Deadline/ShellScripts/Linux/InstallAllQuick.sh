b4746cf2a406741781052cdc07a39df10ded246e
Edwin Amsler <EdwinGuy@GMail.com>
2012-06-28 14:57:10 -0500

    Installation:
    - Now downloads Deadline 5.2 64bit
    
    I really need to make this use the `uname -i` value.

--- Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
+++ Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
@@ -2,7 +2,7 @@
 
 pushd /tmp
 
-curl -L http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5147014-april-17-2012/Deadline_Linux_Installers_x86_64_5_1_47014.tar | tar x
+curl -L http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5247700-june-27-2012/Deadline_Linux_Installers_x86_64_5_2_47700.tar | tar x
 
 DeadlineRepos*	&& rm DeadlineRepos*
 DeadlineClient*	&& rm DeadlineClient*	
