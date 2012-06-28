d7c92c0ef9b937a8159ba40b716f0f7725932446
Edwin Amsler <EdwinGuy@GMail.com>
2012-06-28 15:09:13 -0500

    uname powers: activate!
    - Renamed the downloads on the web site so I could use the 'uname' arch to pick the right download
    - Changed this script to actually pull from that address

--- Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
+++ Thinkbox/Deadline/ShellScripts/Linux/InstallAllQuick.sh
@@ -2,7 +2,7 @@
 
 pushd /tmp
 
-curl -L http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5247700-june-27-2012/Deadline_Linux_Installers_x86_64_5_2_47700.tar | tar x
+curl -L http://www.thinkboxsoftware.com/deadline-downloads/deadline-release-5247700-june-27-2012/Deadline_Linux_Installers_`uname -i`_5_2_47700.tar | tar x
 
 DeadlineRepos*	&& rm DeadlineRepos*
 DeadlineClient*	&& rm DeadlineClient*	
